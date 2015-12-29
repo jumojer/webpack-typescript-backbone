1d3ebf4b43a01d651cfd5ee712c379dc98e5e76f
Javier <javi@javisr.net>
2015-12-29 13:48:45 +0100

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+/// <reference path="../typings/tsd.d.ts" />
+
+import $ = require("jquery");
+import UserView = require("./view/UserView");
+import UserModel = require("./model/UserModel");
+
+function app() {
+	$("body").append(new UserView({model : new UserModel()}).render().$el);
+}
+
+export = app;
